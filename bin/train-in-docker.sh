d1066d6a7c03c818f8d7b442fcedd9bccaa0626b
Cameron Martin <cameronmartin123@gmail.com>
2020-07-10 00:40:34 +0100

    Fix script for running in docker

--- bin/train-in-docker.sh
+++ bin/train-in-docker.sh
@@ -2,6 +2,8 @@
 
 set -eo pipefail
 
+mkdir -p tmp
+
 IMAGE_ID_FILE=tmp/docker-image-id
 CONTAINER_ID_FILE=tmp/docker-container-id
 
@@ -15,7 +17,7 @@ fi
 IMAGE_ID="$(cat "$IMAGE_ID_FILE")"
 
 if [ ! -f "$CONTAINER_ID_FILE" ]; then
-    docker create -u $USER_ID:$GROUP_ID -v $(realpath .):/project --workdir /project --gpus all --cidfile "$CONTAINER_ID_FILE" "$IMAGE_ID" ./bin/run-container.sh
+    docker create -it -u $USER_ID:$GROUP_ID -v $(realpath .):/project --workdir /project --gpus all --cidfile "$CONTAINER_ID_FILE" "$IMAGE_ID" ./bin/run-container.sh
 fi
 
 CONTAINER_ID="$(cat "$CONTAINER_ID_FILE")"
